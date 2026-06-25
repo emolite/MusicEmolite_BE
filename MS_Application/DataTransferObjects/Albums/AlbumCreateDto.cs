9e5683514b239adf399f0dea125fe1ad1d3f839f
baovq <baovq@amfcorp.com>
2026-06-25 22:44:44 +0700

    update api album

--- MS_Application/DataTransferObjects/Albums/AlbumCreateDto.cs
+++ MS_Application/DataTransferObjects/Albums/AlbumCreateDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,5 +16,7 @@ namespace MS_Application.DataTransferObjects.Albums
         public long? ArtistId { get; set; }
 
         public byte AlbumType { get; set; }
+
+        public IFormFile Image { get; set; }
     }
 }
